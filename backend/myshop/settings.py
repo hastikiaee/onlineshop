b9db8008ee0734a4822af173a224724f8536555d
hastikiaee <hasti1383kiaee@gmail.com>
2026-05-30 23:01:51 +0430

    fix secret key

--- backend/myshop/settings.py
+++ backend/myshop/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "test-secret-key-not-for-prod")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")=="1"
