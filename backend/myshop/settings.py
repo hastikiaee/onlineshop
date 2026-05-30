02ba364c71f4f40badeb1b94a89fa467e0fa34b5
hastikiaee <hasti1383kiaee@gmail.com>
2026-05-30 23:46:47 +0430

    using python decouple

--- backend/myshop/settings.py
+++ backend/myshop/settings.py
@@ -86,14 +86,16 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),  # خیلی مهم
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": config("POSTGRES_DB", default="myshop"),
+        "USER": config("POSTGRES_USER", default="postgres"),
+        "PASSWORD": config("POSTGRES_PASSWORD", default="postgres"),
+        "HOST": config("POSTGRES_HOST", default="db"),
+        "PORT": config("POSTGRES_PORT", default=5432, cast=int),
     }
 }
 
